4e2e1ccf0389bcb3eb0753f54a51b4d20d5f70d7
Moni Manolov <monimanolov@livewallgroup.com>
2022-05-28 12:32:21 +0200

    Google cloud project changed

--- src/main/java/com/authservice/authservice/conf/WebSecurityConfig.java
+++ src/main/java/com/authservice/authservice/conf/WebSecurityConfig.java
@@ -4,9 +4,7 @@ import com.authservice.authservice.auth.JwtAuthenticationEntryPoint;
 import com.authservice.authservice.auth.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,8 +16,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 @EnableWebSecurity
